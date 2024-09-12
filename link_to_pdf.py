9ef7bc128a07ebc1aa4a88414796e208abb8def7
Piotr Górny <84388562+nadziejapio@users.noreply.github.com>
2024-09-12 10:13:37 +0200

    Update link_to_pdf.py

--- link_to_pdf.py
+++ link_to_pdf.py
@@ -2,7 +2,7 @@ from PyPDF2 import PdfReader
 from PyPDF2.generic import AnnotationBuilder
 from PyPDF2 import PdfWriter
 
-with open("cv2024.pdf", "rb") as pdf_file:
+with open("file.pdf", "rb") as pdf_file:
     pdf_reader = PdfReader(pdf_file)
     page_number = 0  
     page = pdf_reader.pages[page_number]
@@ -12,12 +12,12 @@ with open("cv2024.pdf", "rb") as pdf_file:
     x, y = 50, 640 
 
     hyperlink = AnnotationBuilder.link(
-        url="https://linkedin.com/in/piogorny/",  # Adres URL, który ma być hiperłączem
-        rect=[x, y, x + 120, y + 20],  # Współrzędne obszaru hiperłącza
+        url="https://", 
+        rect=[x, y, x + 120, y + 20],
     )
 
     hyperlink_2 = AnnotationBuilder.link(
-        url = "https://github.com/nadziejapio",
+        url = "https://",
         rect = [x, y-23, x+100, y-3]
     )
     output_pdf.add_annotation(page_number=0, annotation = hyperlink)
